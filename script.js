7a84761c498f37b2b38e6281222979f8a24cd3f0
RuAleksandr <RuAleksandr07@gmail.com>
2022-12-14 21:29:08 +0500

    Урок 24. Условия и циклы

--- script.js
+++ script.js
@@ -8,12 +8,50 @@ const personalMovieDB = {
     privat: false,
 };
 
-const a = prompt("Один из последних просмотренных фильмов?","Один дома"),
-      b = prompt("На сколько оцените его?", "10"),
-      c = prompt("Один из последних просмотренных фильмов?","Один дома"),
-      d = prompt("На сколько оцените его?", "10");
+if(personalMovieDB.count < 10) {
+    alert('Просмотрено довольно мало фильмов');
+} else if(personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    alert('Вы класический зритель');
+} else if(personalMovieDB.count > 30) {
+    alert('Вы киноман');
+} else {
+    alert('Произошла ошибка');
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for(let i = 0; i <= 1; i++) {
+    let a = prompt("Один из последних просмотренных фильмов?","Один дома"),
+        b = prompt("На сколько оцените его?", "10");
+    if(a && b && a.length <= 50) {
+        personalMovieDB.movies[a] = b;
+    } else {
+        i--;
+    }
+}
+
+// // Переписать цикл: 1 способ
+// let countQuetion = 2;
+// while(countQuetion > 0) {
+//     let a = prompt("Один из последних просмотренных фильмов?","Один дома"),
+//         b = prompt("На сколько оцените его?", "10");
+//     if(a && b && a.length <= 50) {
+//         personalMovieDB.movies[a] = b;
+//     } else {
+//         countQuetion++;
+//     }
+//     countQuetion--;
+// }
+
+// Переписать цикл: 2 способ
+// let countQuetion = 1;
+// do {
+//     let a = prompt("Один из последних просмотренных фильмов?","Один дома"),
+//         b = prompt("На сколько оцените его?", "10");
+//     if(a && b && a.length <= 50) {
+//         personalMovieDB.movies[a] = b;
+//     } else {
+//         countQuetion++;
+//     }
+//     countQuetion--;
+// } while (countQuetion > 0)
 
 console.log(personalMovieDB);

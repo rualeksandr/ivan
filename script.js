768749c29e93c90f42c2eeb9e4143f8f472c9038
rualeksandr <45438705+rualeksandr@users.noreply.github.com>
2022-12-16 16:24:23 +0500

    Урок 29. Использование функций

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "20");
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -8,25 +16,45 @@ const personalMovieDB = {
     privat: false,
 };
 
-if(personalMovieDB.count < 10) {
-    alert('Просмотрено довольно мало фильмов');
-} else if(personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-    alert('Вы класический зритель');
-} else if(personalMovieDB.count > 30) {
-    alert('Вы киноман');
-} else {
-    alert('Произошла ошибка');
-}
-
-for(let i = 0; i <= 1; i++) {
-    let a = prompt("Один из последних просмотренных фильмов?","Один дома"),
-        b = prompt("На сколько оцените его?", "10");
-    if(a && b && a.length <= 50) {
-        personalMovieDB.movies[a] = b;
-    } else {
-        i--;
+function rememberMyFilms() {
+    for(let i = 0; i <= 1; i++) {
+        let a = prompt("Один из последних просмотренных фильмов?","Один дома"),
+            b = prompt("На сколько оцените его?", "10");
+        if(a && b && a.length <= 50) {
+            personalMovieDB.movies[a] = b;
+        } else {
+            i--;
+        }
     }
 }
+// rememberMyFilms();
+
+function detectPersonalLevel() {
+    if(personalMovieDB.count < 10) {
+        alert('Просмотрено довольно мало фильмов');
+    } else if(personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+        alert('Вы класический зритель');
+    } else if(personalMovieDB.count > 30) {
+        alert('Вы киноман');
+    } else {
+        alert('Произошла ошибка');
+    }
+}
+// detectPersonalLevel();
+
+function showMyDB(hidden) {
+    if(!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+    for(let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр ${i}`,"Хоррор");
+    }
+}
+writeYourGenres()
 
 // // Переписать цикл: 1 способ
 // let countQuetion = 2;
@@ -53,5 +81,3 @@ for(let i = 0; i <= 1; i++) {
 //     }
 //     countQuetion--;
 // } while (countQuetion > 0)
-
-console.log(personalMovieDB);

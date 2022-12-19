580fe92aaab68b1b70fd67d147c39d47b24c8a45
RuAleksandr <RuAleksandr07@gmail.com>
2022-12-19 23:33:47 +0500

    Урок 37. Используем объекты

--- script.js
+++ script.js
@@ -1,83 +1,65 @@
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-    }
-}
-start();
+'use strict'
 
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
     privat: false,
+    start: function() {
+        personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+        }
+    },
+    rememberMyFilms: function() {
+        for(let i = 0; i <= 1; i++) {
+            let a = prompt("Один из последних просмотренных фильмов?","Один дома").trim(),
+                b = prompt("На сколько оцените его?", "10");
+            if(a && b && a.length <= 50) {
+                personalMovieDB.movies[a] = b;
+            } else {
+                i--;
+            }
+        }
+    },
+    detectPersonalLevel: function() {
+        if(personalMovieDB.count < 10) {
+            alert('Просмотрено довольно мало фильмов');
+        } else if(personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+            alert('Вы класический зритель');
+        } else if(personalMovieDB.count > 30) {
+            alert('Вы киноман');
+        } else {
+            alert('Произошла ошибка');
+        }
+    },
+    toggleVisibleMyDB: function() {
+        if (personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
+        }
+    },
+    showMyDB: function(hidden) {
+        if(!hidden) {
+            console.log(personalMovieDB);
+        }
+    },
+    writeYourGenres: function() {
+        for(let i = 1; i <= 3; i++) {
+            let genre = prompt(`Ваш любимый жанр ${i}`,"Хоррор");
+            
+            if(genre === '' || genre == null) {
+                console.log("Вы ввели некорректные данные или не вели их вовсе");
+                i--;
+            } else {
+                personalMovieDB.genres[i - 1] = genre;
+            }
+        }
+        personalMovieDB.genres.forEach((item, i) => {
+            console.log(`Любимый жанр ${i + 1} - это ${item}`);
+        });
+    },
 };
 
-function rememberMyFilms() {
-    for(let i = 0; i <= 1; i++) {
-        let a = prompt("Один из последних просмотренных фильмов?","Один дома"),
-            b = prompt("На сколько оцените его?", "10");
-        if(a && b && a.length <= 50) {
-            personalMovieDB.movies[a] = b;
-        } else {
-            i--;
-        }
-    }
-}
-// rememberMyFilms();
-
-function detectPersonalLevel() {
-    if(personalMovieDB.count < 10) {
-        alert('Просмотрено довольно мало фильмов');
-    } else if(personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-        alert('Вы класический зритель');
-    } else if(personalMovieDB.count > 30) {
-        alert('Вы киноман');
-    } else {
-        alert('Произошла ошибка');
-    }
-}
-// detectPersonalLevel();
-
-function showMyDB(hidden) {
-    if(!hidden) {
-        console.log(personalMovieDB);
-    }
-}
-showMyDB(personalMovieDB.privat);
-
-function writeYourGenres() {
-    for(let i = 1; i <= 3; i++) {
-        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр ${i}`,"Хоррор");
-    }
-}
-writeYourGenres()
-
-// // Переписать цикл: 1 способ
-// let countQuetion = 2;
-// while(countQuetion > 0) {
-//     let a = prompt("Один из последних просмотренных фильмов?","Один дома"),
-//         b = prompt("На сколько оцените его?", "10");
-//     if(a && b && a.length <= 50) {
-//         personalMovieDB.movies[a] = b;
-//     } else {
-//         countQuetion++;
-//     }
-//     countQuetion--;
-// }
-
-// Переписать цикл: 2 способ
-// let countQuetion = 1;
-// do {
-//     let a = prompt("Один из последних просмотренных фильмов?","Один дома"),
-//         b = prompt("На сколько оцените его?", "10");
-//     if(a && b && a.length <= 50) {
-//         personalMovieDB.movies[a] = b;
-//     } else {
-//         countQuetion++;
-//     }
-//     countQuetion--;
-// } while (countQuetion > 0)
